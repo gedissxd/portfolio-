1921c484fe7d7cdbd735d7e5097346a2d50108c1
Gediminas <gediss.lolas78@gmail.com>
2025-02-06 17:16:49 +0200

    make it right design

--- views/index.view.php
+++ views/index.view.php
@@ -10,18 +10,18 @@
 <h1 class="font-bold text-3xl flex justify-center items-center mt-5">
   Leave a comment
 </h1>
-<form  method="POST" class="mx-auto w-2xl mt-5 bg-gray-400">
+<form  method="POST" class="mx-auto w-2xl mt-10">
 
  <div class="flex justify-between">
- <label class="p-2">Email*</label> <input class="bg-red-300 rounded-md p-2" type="text" name="name">
- <label class="p-2">Name*</label> <input class="bg-red-300 rounded-md p-2" type="text" name="email">
+ <label class="p-2">Email*</label> <input class="border-1 border-gray-400 rounded-md p-2" type="text" name="name">
+ <label class="p-2">Name*</label> <input class="border-1 border-gray-400 rounded-md p-2" type="text" name="email">
  </div>
 
 <div class="flex mt-3">
-    <label class="p-2 mr-5 mt-7">Comment*</label>
-    <textarea class="bg-red-300 h-32 w-full rounded-md" type="text" name="comment"></textarea>
+    <label class="p-2 mr-4 mt-7">Comment*</label>
+    <textarea class="border-1 border-gray-400 h-32 w-full rounded-md" type="text" name="comment"></textarea>
 </div>
-<input class="bg-gray-700 px-3 py-1 rounded-md mt-3 ml-28" type="submit">
+<input class="bg-gray-400 px-4 py-2 rounded-md mt-3 ml-27" type="submit">
 
 
 
