6bb2f5600c8d42c8089055dfb023dba9a5bcfd72
Gediminas <gediss.lolas78@gmail.com>
2025-02-06 16:58:49 +0200

    make form template

--- views/index.view.php
+++ views/index.view.php
@@ -5,9 +5,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./output.css" rel="stylesheet">
 </head>
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<body class="bg-gray-300">
+
+<h1 class="font-bold text-3xl flex justify-center items-center mt-5">
+  Leave a comment
+</h1>
+<form  method="POST" class="mx-auto w-2xl mt-5 bg-gray-400">
+
+ <div class="flex justify-between">
+ <label class="p-2">Email*</label> <input class="bg-red-300 rounded-md p-2" type="text" name="name">
+ <label class="p-2">Name*</label> <input class="bg-red-300 rounded-md p-2" type="text" name="email">
+ </div>
+
+<div class="flex mt-3">
+    <label class="p-2 mr-5 mt-7">Comment*</label>
+    <textarea class="bg-red-300 h-32 w-full rounded-md" type="text" name="comment"></textarea>
+</div>
+<input class="bg-gray-700 px-3 py-1 rounded-md mt-3 ml-28" type="submit">
+
+
+
+
+</form>
 </body>
 </html>
