3c19dba6b30708c94b3ef316d6557d1cd5629404
Gediminas <gediss.lolas78@gmail.com>
2025-02-07 15:05:41 +0200

    show reply only for comments not replies

--- views/components/comment.php
+++ views/components/comment.php
@@ -4,13 +4,15 @@
       <div class="font-bold mr-2"><?php echo $comment['name']; ?></div>
       <div><?php echo date('d M Y', strtotime($comment['created_at'])); ?></div>
     </div>
-    <div class="flex items-center cursor-pointer" onclick="showReplyForm(<?php echo $comment['id']; ?>)">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="size-6 mr-1">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m15 15 6-6m0 0-6-6m6 6H9a6 6 0 0 0 0 12h3" />
-      </svg>
-      <span>Reply</span>
-    </div>
+    <?php if (!isset($comment['parent_id']) || $comment['parent_id'] == 0): ?>
+      <div class="flex items-center cursor-pointer" onclick="showReplyForm(<?php echo $comment['id']; ?>)">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="size-6 mr-1">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m15 15 6-6m0 0-6-6m6 6H9a6 6 0 0 0 0 12h3" />
+        </svg>
+        <span>Reply</span>
+      </div>
+    <?php endif; ?>
   </div>
   <div class="mt-2">
     <h2><?php echo $comment['comment']; ?></h2>
