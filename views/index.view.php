77fc95245504af39a56fedf209c5a8d3eb5bf266
Gediminas <gediss.lolas78@gmail.com>
2025-02-11 18:57:02 +0200

    use tailwindcli

--- views/index.view.php
+++ views/index.view.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./output.css" rel="stylesheet">
-  <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
     crossorigin="anonymous"></script>
 
